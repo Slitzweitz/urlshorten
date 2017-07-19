13528f3bf112e055fa51d8d5d75c8514f559e063
Colin Barlow <colinmbarlow@gmail.com>
2017-07-19 18:49:31 +0000

    scrapped the validation, went for the multiple shortenkeys for each url. inserts a url every time, gives uniquely indexed 4 digit key

--- server.js
+++ server.js
@@ -2,19 +2,19 @@
 //  If I pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain an error instead.
 //  When I visit that shortened URL, it will redirect me to my original link.
   //  if it is a url:
-  //  - check to see if it has already been shortened, by searching the database for the url (req.params[0])
-  //    > if it is new, shorten it and store it. 
-  //    > if it is in the database already, use stored version to redirect
-  //  - make sure random number has not been used on another link
-  //  - display an object with the original and shortened urls
-//  When a get request is made to /new/:url, after checking that its feasibly a url, 
-//  store the link in a collection in a mongodb database
+  //  - shorten it (generate random 4 digit, uniquely indexed number)
+  //  - insert it
+  //  if it is a 4 digit number:
+  //  - search the db for it
+  //  - pass the url to be redirected 
+  //  - redirect
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8080;
-const mongo = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017/urlKeys';
+const express = require('express'),
+      app = express(),
+      port = process.env.PORT || 8080,
+      mongo = require('mongodb').MongoClient,
+      url = 'mongodb://localhost:27017/urlKeys',
+      test = require('assert');
 
 //  accept all characters, including /
 app.get('/*', function(req, res) {
@@ -22,62 +22,95 @@ app.get('/*', function(req, res) {
   //  store the url which was passed, as a string
   var urlToShorten = req.params[0];
   var randomNumber = Math.floor(1000 + Math.random() * 9000);
-
+  var upDoc = {
+    passedurl : urlToShorten,
+    shortenkey : randomNumber,
+    shortenedurl : 'https://ran-dom-heroku.com/' + randomNumber
+  };
   //  regex to make sure it is a url
   var urlRegex = /https?\:\/\/(www)?(\w+.){2,}\w+(\/.+)?/g;
+  var shortRegex = /\d{4}/g;
 
   if (urlRegex.test(urlToShorten)) {
-  //  - check to see if it has already been shortened, by searching the database
-  //  use callback to connect to db, insert info
-  
     mongo.connect(url, function(err, db) {
       if (err) throw err;
-      console.log('connected to db');
       var collection = db.collection('urlKeys');
-      //  use collection.update with upsert set to true,
-      //  only updates if there are NO matches with the query,
-      //  so if the url passed in does NOT exist, update
-      collection.update({
-        passedurl : urlToShorten
-      }, {
-        passedurl : urlToShorten,
-        shortenkey : randomNumber,
-        shortenedurl : 'https://ran-dom-heroku.com/' + randomNumber
-      }, 
-      {upsert:true},
-      function(err, docs) {
-        if (err) throw err;
-        console.log('updated db', docs);
-        var updated = docs.message;
-        res.send({ 
-          updated
+      
+      insertDb(function(data) {
+        console.log(data.ops[0].shortenedurl);
+        res.send({
+          'shortenedurl' : data.ops[0].shortenedurl,
+          'passedurl' : data.ops[0].passedurl
         });
       });
-      //  Another way: use find() and insert()
+      
+      function insertDb(callback) {
+        console.log('inserting url');
+        collection.insertOne(upDoc, function(err, doc) {
+          test.equal(null, err);
+          test.equal(1, doc.insertedCount, 'try again');
+          console.log('inserted doc');
+          callback(doc);
+          // db.close();
+        });
+      }
       db.close();
     });
-    console.log(urlToShorten + ' is a url');
-    //  also, store it in a database
+  }
+  else if (shortRegex.test(urlToShorten)) {
+    // is already in db
+    mongo.connect(url, function(err, db) {
+      if (err) throw err;
+      console.log('connected to db, for redirection');
+      var collection = db.collection('urlKeys');
+      
+      redirUrl(function(data) {
+        var sending = data.passedurl;
+        res.redirect(sending);
+      });
+      
+      function redirUrl(callback) {
+        collection.find({shortenkey : urlToShorten}, function(err, docs) {
+          test.equal(null, err);
+          console.log(docs + 'key found, redirecting...');
+          callback(docs);
+        });
+      }
+      db.close();
+    });
   }
   else {
     res.send({ 
       'url to be shortened' : urlToShorten + ' is not a valid URL!'
     });
   }
-  
 });
-  // if (req.params.url.test(/https?\:(\/\/){2}(www)?(\w+.){2,}\w+(\/.+)?/g)) {
-  //   console.log(req.params.url);
-  // }
-  // mongo.connect(url, function(err, db) {
-  //   if (err) throw err;
-  //   var collection = db.collection('name');
-  //   collection.update({
-  //     'passed url' : req.params.url
-  //   });
-  //   db.close();
-  // });
-  
-// });
-
 app.listen(port);
+
+// function insertDoc(db, callback) {
+      //   collection.insertOne(upDoc, function(err, result) {
+      //     assert.equal(err, null);
+      //     callback(result.ips);
+      //   });
+      // }
+
+
+//  use collection.update with upsert set to true,
+//  only updates if there are NO matches with the query,
+//  so if the url passed in does NOT exist, update
+// collection.update({
+//   passedurl : urlToShorten
+// }, {
+//   passedurl : urlToShorten,
+//   shortenkey : randomNumber,
+//   shortenedurl : 'https://ran-dom-heroku.com/' + randomNumber
+// }, 
+// {upsert:true},
+// function(err, docs) {
+//   if (err) throw err;
+//   console.log('updated db', docs);
+//   var updated = docs.message;
+//   res.send({ 
+//     updated
+//   });
+// });
