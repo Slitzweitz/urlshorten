818ad6d0b4e0864297c508967d114d777f55a05d
Colin Barlow <colinmbarlow@gmail.com>
2017-07-21 06:12:15 +0000

    updated for heroku integration, hopefully v1.1

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const express = require('express'),
       port = process.env.PORT || 8080,
       mongo = require('mongodb').MongoClient,
       options = require('./options'),
-      url = 'mongodb://' + options.storageConfig.user + ':' + options.storageConfig.password + '@ds047305.mlab.com:47305/heroku_q3hjrckj',
+      url = 'mongodb://heroku_q3hjrckj:4nrhsu1dfjmrvcva2ta6p4s9u6@ds047305.mlab.com:47305/heroku_q3hjrckj',
       test = require('assert');
 
 //  accept all characters, including /
